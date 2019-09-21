9c6d7f1e2f82dd01717a6800f1d8f29163293ffb
naruebate27 <naruebate27@gmail.com>
2019-09-21 16:16:13 +0700

    switch case

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,15 @@
-class FizzBuzz{
-    static say(number){
-        if(number %5 == 0 && number %3 == 0){
-            return "FizzBuzz"
-        }else if(number %5 == 0){
-            return "Buzz"
-        }else if(number %3 == 0){
-            return "Fizz"
-        }
-        return number; 
+class FizzBuzz {
+  static say(number) {
+    switch (true) {
+      case number % 5 == 0 && number % 3 == 0:
+        return "FizzBuzz";
+      case number % 5 == 0:
+        return "Buzz";
+      case number % 3 == 0:
+        return "Fizz";
+      default:
+        return number;
     }
+  }
 }
 module.exports = FizzBuzz;
